8d031c82f20274bac476a658f2d813279e644820
Italo-Diass <italogabrielsiqueira02@gmail.com>
2024-07-07 20:48:17 -0300

    feat clear/del/eq

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,50 @@
 function Calculadora() {
     this.display = document.querySelector('.display');
 
+    this.inicia = () => {
+        this.capturaCliques();
+        this.capturaEnter();  
+    };
+
+    this.capturaEnter = () => {
+        document.addEventListener('keypress', e => {
+            if (e.keyCode === 13) {
+                this.realizaConta();
+            }
+        });
+    };
+
     this.capturaCliques = () => {
         document.addEventListener('click', event => {
             const el = event.target;
-            if (el.classListcontains('btn-num')) this.addNumDisplay(el);
-            if (el.classListcontains('btn-clear')) this.clear(el);
-            if (el.classListcontains('btn-del')) this.del(el);
-            if (el.classListcontains('btn-eq')) this.realizaConta(el);
+            if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+            if (el.classList.contains('btn-clear')) this.clear(el);
+            if (el.classList.contains('btn-del')) this.del(el);
+            if (el.classList.contains('btn-eq')) this.realizaConta(el);
 
         });
     };
 
-    this.addNumDisplay = el => this.display.value += el.innerText;
+    this.addNumDisplay = el => { 
+        this.display.value += el.innerText;
+        this.display.focus(); 
+    };
+    this.clear = () => this.display.value = '';
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
+    this.realizaConta = () => {
+        try {
+            const conta = eval(this.display.value);
 
-    this.inicia = () => {
-        this.capturaCliques();
+            if(!conta) {
+                alert('Conta inválida');
+                return;
+            }
+
+            this.display.value = conta;
+        }catch(e){
+            alert('Conta inválida')
+            return;
+        }
     };
 }
 
